8a8be51972eea27c984b19a3d3231b0c6ec632b7
az-my <83208476+az-my@users.noreply.github.com>
2025-01-16 12:50:09 +0700

    Update sppd-frontend-report-pln.js

--- sppd-frontend-report-pln.js
+++ sppd-frontend-report-pln.js
@@ -55,7 +55,7 @@ const fetchData = async () => {
           TanggaMulai: row[9],
           TanggaMulaiDate: date,
           TanggalSelesai: row[10],
-          PenandaTangan: row[2],
+          PenandaTangan: row[1],
           PejabatPemberiTugas: pejabatPemberiTugas,
           Tujuan: row[6],
           JumlahSPPD: parseFloat(row[17]?.replace(/\./g, '') || 0),
