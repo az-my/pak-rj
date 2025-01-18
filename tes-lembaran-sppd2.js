482b0909353dc4f5c37d0054d6479431288d0441
az-my <83208476+az-my@users.noreply.github.com>
2025-01-18 14:01:07 +0700

    Update tes-lembaran-sppd2.js

--- tes-lembaran-sppd2.js
+++ tes-lembaran-sppd2.js
@@ -16,193 +16,233 @@ document.addEventListener('DOMContentLoaded', async () => {
     const generateReportHTML = (row) => {
         const jabatanDriver = "Manager ULTG Banda Aceh"; // Example position, replace with actual data if available
         return `
-<div id="report-${row[0]}" class="p-6 page-break text-xs">
-  <div class="mb-6">
-    <h1 class="font-bold">PT. PALMA NAFINDO PRATAMA</h1>
-    <h2 class="font-bold">BANDA ACEH</h2>
-  </div>
+            <div id="report-${row[0]}" class="w-full p-6 page-break text-xs rounded-lg mb-6">
+                <div class="mb-6">
+                    <h1 class="font-bold">PT. PALMA NAFINDO PRATAMA</h1>
+                    <h2 class="font-bold">BANDA ACEH</h2>
+                </div>
+                <h1 class="text-center underline font-bold text-lg mb-6">SURAT PERINTAH PERJALANAN DINAS (SPPD)</h1>
+                <div class="border border-black rounded-lg">
+                    <table class="w-full border-collapse">
+                        <tbody>
+                            <!-- Points 1 to 6 -->
+                            <tr class="border-b border-black">
+                                <td class="border-r border-black p-2 w-auto">1.</td>
+                                <td class="border-r border-black p-2 w-1/2">Pegawai yang di Perintah</td>
+                                <td class="p-2 w-1/2">
+                                    <div>Nama: <span>${row[2]}</span></div>
+                                    <div>Jabatan: <span>Driver</span></div>
+                                </td>
+                            </tr>
+                            <tr class="border-b border-black">
+                                <td class="border-r border-black p-2 w-auto">2.</td>
+                                <td class="border-r border-black p-2 w-1/2">Maksud Perjalanan Dinas</td>
+                                <td class="p-2 w-1/2"><span>${row[8]}</span></td>
+                            </tr>
+                            <tr class="border-b border-black">
+                                <td class="border-r border-black p-2 w-auto">3.</td>
+                                <td class="border-r border-black p-2 w-1/2">Alat angkutan yang dipergunakan</td>
+                                <td class="p-2 w-1/2"><span>${row[7]}</span></td>
+                            </tr>
+                            <tr class="border-b border-black">
+                                <td class="border-r border-black p-2 w-auto">4.</td>
+                                <td class="border-r border-black p-2 w-1/2">
+                                    <div>a. Tempat berangkat (tempat kedudukan)</div>
+                                    <div>b. Tempat Tujuan</div>
+                                </td>
+                                <td class="p-2 w-1/2">
+                                    <div>a. <span>${row[3]}</span></div>
+                                    <div>b. <span>${row[6]}</span></div>
+                                </td>
+                            </tr>
+                            <tr class="border-b border-black">
+                                <td class="border-r border-black p-2 w-auto">5.</td>
+                                <td class="border-r border-black p-2 w-1/2">
+                                    <div>a. Lama perjalanan dinas</div>
+                                    <div>b. Tanggal berangkat</div>
+                                    <div>c. Tanggal kembali</div>
+                                </td>
+                                <td class="p-2 w-1/2">
+                                    <div>a. <span>${row[11]} Hari</span></div>
+                                    <div>b. <span>${moment(row[9], 'DD/MM/YYYY').locale('id').format('DD MMMM YYYY')}</span></div>
+                                    <div>c. <span>${moment(row[10], 'DD/MM/YYYY').locale('id').format('DD MMMM YYYY')}</span></div>
+                                </td>
+                            </tr>
+                            <tr class="border-b border-black">
+                                <td class="border-r border-black p-2 w-auto">6.</td>
+                                <td class="border-r border-black p-2 w-1/2">
+                                    <div>Biaya</div>
+                                    <div>a. Jumlah</div>
+                                    <div>b. Penanggung</div>
+                                </td>
+                                <td class="p-2 w-1/2">
+                                    <div>&nbsp;</div>
+                                    <div>Rp <span>${row[17].replace(/,\d+$/, '')}</span></div>
+                                    <div>PT. PALMA NAFINDO PRATAMA</div>
+                                </td>
+                            </tr>
+    
+                            <!-- Point 7 -->
+                            <tr class="border-b border-black">
+                                <td class="border-r border-black p-2 w-auto align-top rowspan="2">7.</td>
+                                <td class="border-r border-black p-2 w-1/2 align-top">
+                                    <span class="block">Rincian biaya</span>
+                                    <span class="block font-bold">1. BIAYA ANGKUTAN</span>
+    
+                                    <!-- Sub-items under BIAYA ANGKUTAN -->
+                                    <span class="block pl-6 invisible">Angkutan 1</span>
+                                    <span class="block pl-6 invisible">Angkutan 2</span>
+                                    <span class="block pl-6 invisible">Angkutan 3</span>
+                                    <span class="block pl-6 invisible">Angkutan 4</span>
+                                    <span class="block pl-6">Airport Tax</span>
+                                    <span class="block pl-6">Biaya Angkutan dari Rumah (PP)</span>
+                                    <span class="block pl-6">Biaya Angkutan dari Rumah (PP)</span>
+                                </td>
+                                <td class="p-2 w-1/2">
+                                    <!-- BIAYA ANGKUTAN TABLE -->
+                                    <table class="w-full border-collapse mt-8">
+                                        <tbody>
 
-  <h1 class="text-center underline font-bold text-lg mb-6">
-    SURAT PERINTAH PERJALANAN DINAS (SPPD)
-  </h1>
+ <tr>
+            <!-- Currency Symbol Column (10%) -->
+            <td class="text-left  w-[10%]">
+                <span class="block">Rp</span>
+                <span class="block">Rp</span>
+                <span class="block">Rp</span>
+                <span class="block">Rp</span>
+                <span class="block">Rp</span>
+                <span class="block">Rp</span>
+                <span class="block">Rp</span>
+            </td>
 
-  <div class="border border-black">
-    <div class="flex border-b border-black">
-      <div class="w-10 border-r border-black p-2">1.</div>
-      <div class="flex-1 border-r border-black p-2">
-        Pegawai yang di Perintah
-      </div>
-      <div class="flex-1 p-2">
-        <div>Nama: ${row[2]}</div>
-        <div>Jabatan: ${jabatanDriver}</div>
-      </div>
-    </div>
+            <!-- Amount Column (30%) -->
+            <td class="text-right  w-[30%]">
+                <span class="block">-</span>
+                <span class="block">-</span>
+                <span class="block">-</span>
+                <span class="block">-</span>
+                <span class="block">-</span>
+                <span class="block">-</span>
+                <span class="block">-</span>
+            </td>
 
-    <div class="flex border-b border-black">
-      <div class="w-10 border-r border-black p-2">2.</div>
-      <div class="flex-1 border-r border-black p-2">
-        Maksud Perjalanan Dinas
-      </div>
-      <div class="flex-1 p-2">${row[8]}</div>
-    </div>
+            <!-- Multiplier Column (15%) -->
+            <td class="text-left  w-[15%]">
+                <span class="block">x 1</span>
+                <span class="block">x 1</span>
+                <span class="block">x 1</span>
+                <span class="block">x 1</span>
+                <span class="block">+ Rp</span>
+                <span class="block">x 1</span>
+                <span class="block">x 1</span>
+            </td>
 
-    <div class="flex border-b border-black">
-      <div class="w-10 border-r border-black p-2">3.</div>
-      <div class="flex-1 border-r border-black p-2">
-        Alat angkutan yang dipergunakan
-      </div>
-      <div class="flex-1 p-2">${row[7]}</div>
-    </div>
+            <!-- Multiplier Percentage Column (15%) -->
+            <td class="text-center  w-[15%]">
+                <span class="block"></span>
+                <span class="block"></span>
+                <span class="block"></span>
+                <span class="block"></span>
+                <span class="block"></span>
+                <span class="block"></span>
+                <span class="block"></span>
+            </td>
 
-    <div class="flex border-b border-black">
-      <div class="w-10 border-r border-black p-2">4.</div>
-      <div class="flex-1 border-r border-black p-2">
-        <div>a. Tempat berangkat (tempat kedudukan)</div>
-        <div>b. Tempat Tujuan</div>
-      </div>
-      <div class="flex-1 p-2">
-        <div>a. ${row[3]}</div>
-        <div>b. ${row[6]}</div>
-      </div>
-    </div>
+            <!-- Result Column (30%) -->
+            <td class="text-right w-[30%]">
+                <span class="block">-</span>
+                <span class="block">-</span>
+                <span class="block">-</span>
+                <span class="block">-</span>
+                <span class="block">-</span>
+                <span class="block">-</span>
+                <span class="block">-</span>
+            </td>
+        </tr>
+                                        
+                                        
+                                            </tbody>
+                                    </table>
+                                </td>
+                            </tr>
+    
+                            <!-- Point 7.2 - UANG HARIAN -->
+                            <tr class="border-b border-black">
+                                <td class="border-r border-black p-2 w-auto align-top"></td>
+                                <td class="border-r border-black p-2 w-1/2 align-top">
+                                    <span class="block font-bold">2. UANG HARIAN</span>
+    
+                                    <!-- Sub-items under UANG HARIAN -->
+                                    <span class="block pl-6">Biaya Harian</span>
+                                    <span class="block pl-6">Biaya Penginapan</span>
+                                </td>
+                                <td class="p-2 w-1/2">
+                                    <!-- UANG HARIAN TABLE -->
+                                    <table class="w-full border-collapse">
+                                        <tbody>
 
-    <div class="flex border-b border-black">
-      <div class="w-10 border-r border-black p-2">5.</div>
-      <div class="flex-1 border-r border-black p-2">
-        <div>a. Lama perjalanan dinas</div>
-        <div>b. Tanggal berangkat</div>
-        <div>c. Tanggal kembali</div>
-      </div>
-      <div class="flex-1 p-2">
-        <div>a. ${row[11]} Hari</div>
-        <div>b. ${row[9]} </div>
-        <div>c. ${row[10]} </div>
-      </div>
-    </div>
+<tr>
+            <!-- Currency Symbol Column (10%) -->
+            <td class="text-left w-[10%]">
+                <span class="block">Rp</span>
+                <span class="block">Rp</span>
+            </td>
 
-    <div class="flex border-b border-black">
-      <div class="w-10 border-r border-black p-2">6.</div>
-      <div class="flex-1 border-r border-black p-2">
-        <div>Biaya</div>
-        <div>a. Jumlah</div>
-        <div>b. Penanggung</div>
-      </div>
-      <div class="flex-1 p-2">
-        <div>&nbsp;</div>
-        <div>Rp ${row[17]}</div>
-        <div>PT. PALMA NAFINDO PRATAMA</div>
-      </div>
-    </div>
+            <!-- Amount Column (30%) -->
+            <td class="text-right  w-[30%]">
+                <span class="block">150.000</span>
+                <span class="block">250.000</span>
+            </td>
 
-    <div class="flex border-b border-black">
-      <div class="w-10 border-r border-black p-2">7.</div>
-      <div class="flex-1 p-2">
-        <div class="mb-4">
-          <div>Rincian biaya</div>
-          <div class="font-bold">1. BIAYA ANGKUTAN</div>
-          <div class="flex ml-4">
-            <div class="flex-1"></div>
-            <div class="w-24">Rp -</div>
-            <div class="w-12">x</div>
-            <div class="w-12">0</div>
-          </div>
-          <div class="flex ml-4">
-            <div class="flex-1"></div>
-            <div class="w-24">Rp -</div>
-            <div class="w-12">x</div>
-            <div class="w-12">0</div>
-          </div>
-          <div class="flex ml-4">
-            <div class="flex-1"></div>
-            <div class="w-24">Rp -</div>
-            <div class="w-12">x</div>
-            <div class="w-12">0</div>
-          </div>
-          <div class="flex ml-4">
-            <div class="flex-1"></div>
-            <div class="w-24">Rp -</div>
-            <div class="w-12">x</div>
-            <div class="w-12">0</div>
-          </div>
-          <div class="flex ml-4">
-            <div class="flex-1">Airport Tax</div>
-            <div class="w-24">Rp -</div>
-            <div class="w-12">x</div>
-            <div class="w-12">0</div>
-          </div>
-          <div class="flex ml-4">
-            <div class="flex-1">Biaya Angkutan dari Rumah (PP)</div>
-            <div class="w-24">Rp -</div>
-            <div class="w-12">x</div>
-            <div class="w-12">1</div>
-          </div>
-        </div>
-        <div>
-          <div class="font-bold">2. UANG HARIAN</div>
-          <div class="flex ml-4">
-            <div class="flex-1">Biaya Harian</div>
-            <div class="w-24">Rp${row[13]}</div>
-            <div class="w-12">x</div>
-            <div class="w-6"></div>
-            <div class="w-12">${row[11]}</div>
-            <div class="w-12">hari</div>
-            <div class="w-12">x</div>
-            <div class="w-12">100%</div>
-          </div>
-          <div class="flex ml-4">
-            <div class="flex-1">Biaya Penginapan</div>
-            <div class="w-24">Rp${row[14]}</div>
-            <div class="w-12">x</div>
-            <div class="w-6"></div>
-            <div class="w-12">0</div>
-            <div class="w-12">hari</div>
-            <div class="w-12">x</div>
-            <div class="w-12">100%</div>
-          </div>
-        </div>
-      </div>
-      <div class="border-l border-black p-2 w-30 text-right">
-        <div>&nbsp;</div>
-        <div>&nbsp;</div>
-        <div class="w-24">-</div>
-        <div class="w-24">-</div>
-        <div class="w-24">-</div>
-        <div class="w-24">-</div>
-        <div class="w-24">-</div>
-        <div class="w-24">-</div>
-        <div class="w-24 mb-4">&nbsp;</div>
-        <div class="w-24">${row[15]}</div>
-        <div class="w-24">${row[16]}</div>
-      </div>
-    </div>
-    <div class="flex justify-space-between border-b border-black">
-      <div class="w-10 border-r border-black">&nbsp;</div>
-      <div class="flex-1 text-center font-bold">TOTAL</div>
-      <div class="w-28 border-l border-black font-bold text-right pr-2">${row[17]}
-        
-      </div>
-    </div>
+            <!-- Multiplier & Days Column (15%) -->
+            <td class="text-center  w-[15%]">
+                <span class="block">x 3 Hari</span>
+                <span class="block">x 0 Hari</span>
+            </td>
 
-    <div class="flex">
-      <div class="w-10 border-r border-black p-2">8.</div>
-      <div class="flex-1 border-r border-black p-2">
-        Keterangan lain - lain
-      </div>
-      <div class="flex-1 p-2"></div>
-    </div>
-  </div>
+            <!-- Multiplier & Percentage Column (15%) -->
+            <td class="text-center  w-[15%]">
+                <span class="block">x 100%</span>
+                <span class="block">x 100%</span>
+            </td>
 
-  <div class="flex justify-end">
-    <div class="w-64 mt-6">
-      <div>Dikeluarkan di: Banda Aceh</div>
-      <div>Pada Tanggal: ${row[9]}</div>
-      <div class="mt-4 text-center">
-        <div class="font-bold">DIREKTUR</div>
-        <div class="mt-16">RIZKY NAHARDI</div>
-      </div>
-    </div>
-  </div>
-</div>
+            <!-- Total Amount Column (30%) -->
+            <td class="text-right  w-[30%]">
+                <span class="block">${row[15].replace(/,\d+$/, '')}</span>
+                <span class="block">${row[16].replace(/,\d+$/, '')}</span>
+            </td>
+        </tr>
+
+                                        </tbody>
+                                    </table>
+                                </td>
+                            </tr>
+    
+                            <!-- TOTAL -->
+                            <tr class="border-b border-black">
+                                <td class="border-r border-black p-2"></td>
+                                <td class="text-center font-bold p-2">TOTAL</td>
+                                <td class="border-l border-black font-bold text-right p-2">${row[17].replace(/,\d+$/, '')}</td>
+                            </tr>
+                                                        <tr class="border-b border-black">
+                                <td class="border-r border-black p-2">8.</td>
+                                <td class="text-center font-bold p-2">Keterangan lain - lain</td>
+                                <td class="border-l border-black font-bold text-right p-2">-</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="flex justify-end">
+                    <div class="w-64 mt-6">
+                        <div>Dikeluarkan di: Banda Aceh</div>
+                        <div>Pada Tanggal&nbsp;&nbsp;: <span>${moment(row[9], 'DD/MM/YYYY').locale('id').format('DD MMMM YYYY')}</span></div>
+                        <div class="mt-4 text-center">
+                            <div class="font-bold">DIREKTUR</div>
+                            <div class="mt-16">RIZKY NAHARDI</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         `;
     };
 
